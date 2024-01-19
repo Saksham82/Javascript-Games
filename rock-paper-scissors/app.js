4782d18f3e85e4d0f383332fb7dea1b09e5d07b7
saksham <saksham82878287@gmail.com>
2024-01-19 13:41:25 +0530

    .

---
+++ rock-paper-scissors/app.js
@@ -0,0 +1,35 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 //or u can use possiblechoices.lenght
+    if(randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if(randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if(randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() 
+{
+    if(computerChoice === userChoice){
+        result = "It's a draw"
+    }
+}
+
