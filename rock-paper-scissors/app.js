3d0772e44393626a2ba0c2d973e2738ade4824d0
saksham <saksham82878287@gmail.com>
2024-01-19 14:00:54 +0530

    rock paper scissors done

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,15 +1,17 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
+const userChoiceDisplay = document.getElementById('your-choice')
 const resultDisplay = document.getElementById('result')
 
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -31,5 +33,25 @@ function getResult()
     if(computerChoice === userChoice){
         result = "It's a draw"
     }
+    if(computerChoice ==='rock' && userChoice === 'paper'){
+        result = "You win"
+    }
+    if(computerChoice ==='rock' && userChoice ==='scissors'){
+        result = "You lost"
+    }
+    if(computerChoice ==='paper' && userChoice ==='scissors'){
+        result = "You win"
+    }
+    if(computerChoice ==='paper' && userChoice ==='rock'){
+        result = "You lost"
+    }
+    if(computerChoice ==='scissors' && userChoice ==='rock'){
+        result = "You win"
+    }
+    if(computerChoice ==='scissors' && userChoice ==='paper'){
+        result = "You lost"
+    }
+    resultDisplay.innerHTML = result
 }
+
 
